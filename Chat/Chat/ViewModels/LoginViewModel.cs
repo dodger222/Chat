0416fb2e9095870456ee2ff2195e995f6220a601
Maxim <sipakovmaksim@gmail.com>
2019-05-28 16:50:05 +0300

    Add LoginViewModel

---
+++ Chat/Chat/ViewModels/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Nickname { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember?")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
