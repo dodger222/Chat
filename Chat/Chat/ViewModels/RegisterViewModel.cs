b2e27bc35dc0c9928c5665a2aac689afa64754fb
Maxim <sipakovmaksim@gmail.com>
2019-05-28 16:14:26 +0300

    Add RegisterViewModel

---
+++ Chat/Chat/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public int Nickname { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password confirm")]
+        public string PasswordConfirm { get; set; }
+    }
+}
