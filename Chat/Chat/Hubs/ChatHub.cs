b832ea20dd966b5f10dc6b5cc4db60df8472eeb8
Maxim <sipakovmaksim@gmail.com>
2019-05-29 09:29:25 +0300

    Save messages to database

--- Chat/Chat/Hubs/ChatHub.cs
+++ Chat/Chat/Hubs/ChatHub.cs
@@ -11,9 +11,28 @@ namespace Chat.Hubs
     [Authorize]
     public class ChatHub : Hub
     {
+        ChatContext _db;
+
+        public ChatHub(ChatContext db)
+            :base()
+        {
+            _db = db;
+        }
+
         public async Task Send(string message, string userName)
         {
-            await Clients.All.SendAsync("Receive", message, userName);
+            Message mes = new Message
+            {
+                Text = message,
+                UserId = _db.Users.Where(u => u.UserName == userName).FirstOrDefault().Id
+            };
+
+            if (mes != null && message.Length != 0)
+            {
+                await _db.Messages.AddAsync(mes);
+                _db.SaveChanges();
+                await Clients.All.SendAsync("Receive", message, userName);
+            }
         }
     }
 }
