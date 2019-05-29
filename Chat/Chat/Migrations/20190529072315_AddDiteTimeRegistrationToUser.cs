d0b312255909e3db22c68bd33172a03a8f98a2e0
Maxim <sipakovmaksim@gmail.com>
2019-05-29 10:26:14 +0300

    Add Migration AddDateTimeRegistrationToUser.

---
+++ Chat/Chat/Migrations/20190529072315_AddDiteTimeRegistrationToUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Chat.Migrations
+{
+    public partial class AddDiteTimeRegistrationToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateTimeRegistration",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateTimeRegistration",
+                table: "AspNetUsers");
+        }
+    }
+}
