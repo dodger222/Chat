463d435f0d40db171704326a1a2ac68d04086026
Maxim <sipakovmaksim@gmail.com>
2019-05-30 18:18:07 +0300

    Add IMessageRepository

---
+++ Chat/Chat/Interfaces/IMessageRepository.cs
@@ -0,0 +1,14 @@
+﻿using Chat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Interfaces
+{
+    public interface IMessageRepository
+    {
+        void Add(Message message);
+        IEnumerable<Message> GetMessages();
+    }
+}
