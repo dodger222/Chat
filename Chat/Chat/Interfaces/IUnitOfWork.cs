71369dbbb5a60a0d3ae21e560ba033fd84288094
Maxim <sipakovmaksim@gmail.com>
2019-05-30 18:59:06 +0300

    Add IUnitOfWork

---
+++ Chat/Chat/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Interfaces
+{
+    public class IUnitOfWork
+    {
+        IUserRepository UserRepository { get; }
+        IMessageRepository MessageRepository { get; }
+    }
+}
