7d92e9b8e752f1227529d1e93b625bf450687016
Maxim <sipakovmaksim@gmail.com>
2019-05-31 15:23:09 +0300

    Add PrivateMessage model

---
+++ Chat/Chat/Models/PrivateMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Models
+{
+    public class PrivateMessage
+    {
+        public int ID { get; set; }
+
+        public string FromUserId { get; set; }
+        public string ToUserId { get; set; }
+        public string Text { get; set; }
+        public DateTime DateTimeOfSend { get; set; }
+
+        public User FromUser { get; set; }
+        public User ToUser { get; set; }
+    }
+}
